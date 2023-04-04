43c827cad910aa6655a4b16aa7860cfc6a5e7f70
kato emma <emmanuelkato39@gmail.com>
2023-04-04 19:34:06 +0300

    configured tailwind ready for use

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.html"],
   theme: {
+    screens: {
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536',
+    },
+    colors:{
+      'greenish': '#15dd15',
+      'bgGrayish': '#e2dfdf'
+    },
+    backgroundImage:{
+
+    },
+    backgroundColor:{
+
+    },
     extend: {},
   },
   plugins: [],
